a1270fd796b185c180d0991ce0c54571fd7e3c03
Christian Schaefer <ch.schaefer@student.uni-tuebingen.de>
2022-04-11 13:08:09 +0200

    Added Snapping to Street

--- game.py
+++ game.py
@@ -1,12 +1,26 @@
-from random import random
+import random
 
 from kivy.app import App
 
-from kivy_garden.mapview import MapView, MapLayer
+from kivy.network.urlrequest import UrlRequest
+
+from kivy_garden.mapview import MapView, MapLayer, MarkerMapLayer, MapMarker, MarkerMapLayer
 from kivy.app import App
 from kivy.graphics import Color, Rectangle
+from kivy.uix.image import Image
 
-from pyproj import Proj, transform
+from pyproj import Transformer
+
+
+def convert_projection(center, delta, factor):
+    transformerGPS2XY = Transformer.from_crs("epsg:4326", "epsg:3857")
+    transformerXY2GPS = Transformer.from_crs("epsg:3857", "epsg:4326")
+    lat, lon = center
+    x, y = transformerGPS2XY.transform(lon, lat)
+    dx, dy = delta
+
+    return transformerXY2GPS.transform(x + factor * dx, y + factor * dy)
+
 
 class GameBox(MapLayer):
     def __init__(self, center, size_m, **kwargs):
@@ -18,14 +32,7 @@ class GameBox(MapLayer):
         self.lower_left = self.project(factor=-0.5)
 
     def project(self, factor):
-        xyProj = Proj("epsg:3857")
-        gpsProj = Proj("epsg:4326")
-
-        lat, lon = self.center
-        x, y = transform(gpsProj, xyProj, lon, lat)
-        dx, dy = self.size_m
-
-        return transform(xyProj, gpsProj, x + factor * dx, y + factor * dy)
+        return convert_projection(self.center, self.size_m, factor=factor)
 
     def reposition(self):
         mapview = self.parent
@@ -61,28 +68,75 @@ class GameBox(MapLayer):
             if _west < west:
                 Rectangle(pos=(0, 0), size=(wx, vy))
 
-            # Left
+            # Right
             if _east > east:
                 Rectangle(pos=(ex, 0), size=(vx-ex, vy))
 
+
+class LightCone(MapLayer):
+    def __init__(self, start, **kwargs):
+        super().__init__(**kwargs)
+        self.cone_center = start
+
+
+class ImageLight(MapMarker):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.source = "light.png"
+        self.width = 50
+        self.anchor_x, self.anchor_y = (0.5, 0.5)
+        self.make_request()
+
+    def make_request(self, url="http://lightnup.schaefer-christian.de:5000/nearest/v1/foot"):
+        request_url = f"{url}/{self.lon},{self.lat}"
+        _ = UrlRequest(url=request_url, on_success=self.snap)
+
+    def snap(self, req, result):
+        if result["code"] == "Ok":
+            location = result["waypoints"][0]["location"]
+            self.lon, self.lat = location
             
 
+class FeatureLayer(MarkerMapLayer):
+    def __init__(self, center, size_m, num_items, **kwargs):
+        super().__init__(**kwargs)
 
+        for _ in range(num_items):
+            dx, dy = size_m
+            dx *= 0.5-random.random()
+            dy *= 0.5-random.random()
+            lat, lon = convert_projection(center=center, delta=(dx,dy), factor=1.0)
+            self.add_widget(ImageLight(lon=lon, lat=lat))
 
+            
 class LevelWidget(MapView):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
         self.level = 1
-        self.center = (9.03, 48.40)
+        self.game_center = (9.03, 48.40)
 
-        self.lon, self.lat = self.center
+        self.lon, self.lat = self.game_center
 
         self.size_m = (500, 500)
         self.zoom = 18
 
-        self.add_layer(GameBox(center=self.center,
-                               size_m=self.size_m),
-                        mode="window")
+        self.feature_layer = None
+
+        self.game_box = GameBox(center=self.game_center, size_m=self.size_m)
+        self.light_cone = LightCone(start=self.center)
+
+        self.add_layer(self.game_box)
+        self.add_layer(self.light_cone)
+
+        self.reseed_features(initial=True)             
+
+    def reseed_features(self, num_features=10, initial=False):
+        if not initial:
+            self.remove_layer(self.feature_layer)
+        self.feature_layer = FeatureLayer(center=self.game_center,
+                                          size_m=self.size_m,
+                                          num_items=num_features)
+        self.add_layer(self.feature_layer, mode="window")
 
 
 class MainApp(App):
